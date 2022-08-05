b0c21f89e69f5dc3c3616e0c3a72d58635f771fc
Austin Carman <abcarman12@gmail.com>
2022-08-05 12:50:37 -1000

    added basic API GET request. Needs to be adjusted

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,11 +1,20 @@
 function MainContainer() {
+  // fetch('https://api.tomorrow.io/v4/timelines?location=-73.98529171943665,40.75872069597532&fields=temperature&timesteps=1h&units=metric&apikey=', {
+  //   method: "GET",
+  //   compress: true,
+  // })
+  //   .then((result) => result.json())
+  //   .then((json) => console.log(json))
+  //   .catch((error) => console.error("error: " + error));
+
   return (
     <div className="main-container">
       <div className="city-search-container">
-        <h2 className="city">Honolulu</h2>
+        <h2 className="city">Honolulu</h2> {/* City - from API response */}
+        {/* Set Input to Local state */}
         <input
           type="text"
-          placeholder="city or zip"
+          placeholder="Search City or Zip"
         />
       </div>
       <div>
