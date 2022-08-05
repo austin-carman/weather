a7b8fc81d1ba6ca576949f26748c9ed76895d3e8
Austin Carman <abcarman12@gmail.com>
2022-08-04 14:52:50 -1000

    basic structure of MainContainer

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,7 +1,24 @@
 function MainContainer() {
   return (
     <div className="main-container">
-      <h2>Container 1</h2>
+      <div className="city-search-container">
+        <h2 className="city">Honolulu</h2>
+        <div className="search-container">Search Icon</div>
+      </div>
+      <div>
+        <div>Weather Icon</div>
+        <div>
+          <h3>75&deg;</h3>
+          <h5>Sunny</h5>
+        </div>
+      </div>
+      <div>
+        <div>Icon</div>
+        <div>
+          <h4>Title</h4>
+          <p>Details</p>
+        </div>
+      </div>
     </div>
   )
 }
