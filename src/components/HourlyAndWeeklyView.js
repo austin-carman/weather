f3bfa9b3f3033dfd3113849ced5ca93e8e2bec38
Austin Carman <abcarman12@gmail.com>
2022-08-05 19:49:01 -1000

    adjusted basic structure of component

--- src/components/HourlyAndWeeklyView.js
+++ src/components/HourlyAndWeeklyView.js
@@ -5,32 +5,8 @@ function HourlyAndWeeklyView() {
         <button>Hourly</button>
         <button>Weekly</button>
       </div>
-      <div>
-        <div> {/* Create new component to construct these divs */}
-          <h4>5am</h4>
-          <div>Icon</div>
-          <h4>71&deg;</h4>
-        </div>
-        <div>
-          <h4>6am</h4>
-          <div>Icon</div>
-          <h4>71&deg;</h4>
-        </div>
-        <div>
-          <h4>7am</h4>
-          <div>Icon</div>
-          <h4>71&deg;</h4>
-        </div>
-        <div>
-          <h4>8am</h4>
-          <div>Icon</div>
-          <h4>71&deg;</h4>
-        </div>
-        <div>
-          <h4>9am</h4>
-          <div>Icon</div>
-          <h4>71&deg;</h4>
-        </div>
+      <div> {/* List container */}
+        {/* Render component for each item - Hourly / Weekly  */}
       </div>
     </div>
   )
