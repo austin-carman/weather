25c0ecf6974a1f6caa236d591fd7227138287b70
Austin Carman <abcarman12@gmail.com>
2022-08-09 13:16:33 -1000

    default weather appearing in MainContainer

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-// import logo from './logo.svg';
-import './App.css';
 import MainContainer from './components/MainContainer';
 import HourlyAndWeeklyView from './components/HourlyAndWeeklyView';
 import { useEffect, useState } from "react";
 import { url, options } from "./data/data";
 
 function App() {
+  // May need to adjust initialState structure
   const initialState = {
     daily: [],
     hourly: [],
   }
   const [weather, setWeather] = useState(initialState);
   useEffect(() => {
-    // fetch(url, options)
-    //   .then(response => response.json())
-    //   .then(response => setWeather({ ...initialState, daily: response.data.timelines[0].intervals, hourly: response.data.timelines[1].intervals }))
-    //   .catch(err => console.error(err));
+    fetch(url, options)
+      .then(response => response.json())
+      .then(response => setWeather({ ...initialState, daily: response.data.timelines[0].intervals, hourly: response.data.timelines[1].intervals }))
+      .catch(err => console.error(err));
   }, []); // API call on first render and when user submits a new location
 
 
--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,23 +1,28 @@
+import { weatherCodeDay, weatherCodeNight } from "../data/data";
+
 function MainContainer(props) {
   const weather = props.weather;
+  // switch to weatherCodeFullDay instead of weatherCodeDay?
+  const weatherCode = weather.daily[0].values.weatherCodeDay;
   return (
     <div className="main-container">
       <div className="city-search-container">
-        <h2 className="city">Honolulu</h2>
-        {/* start input as seach Icon -> onClick show input field */}
         <input
           type="text"
           placeholder="Search City or Zip"
         />
+        <h2 className="city">Honolulu</h2>
+        {/* start input as seach Icon -> onClick show input field */}
       </div>
       <div>
         <div>Weather Icon</div>
-        <h4>{weather.daily[0].values.weatherCodeDay}</h4>
+        <h4>{weatherCodeDay[weatherCode]}</h4>
       </div>
       <div>
-        <h3>{weather.hourly[0].values.temperature}&deg;</h3>
-        <h5>{weather.daily[0].values.temperatureMax}</h5>
-        <h5>{weather.daily[0].values.temperatureMin}</h5>
+        {/* Change hardcoded hourly/daily index into dynamic index when appropriate */}
+        <h3>{Math.round(weather.hourly[0].values.temperature)}&deg;</h3>
+        <h5>Low: {Math.round(weather.daily[0].values.temperatureMin)}&deg;</h5>
+        <h5>High: {Math.round(weather.daily[0].values.temperatureMax)}&deg;</h5>
       </div>
       <div>
         {/* list container for Additional weather detail options. Render each item component here */}
