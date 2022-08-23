f3ada8bc54d07df3caca61589796f3ae0fe24cb8
Austin Carman <abcarman12@gmail.com>
2022-08-23 08:45:20 -1000

    restructured state from API to pass to components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import CurrentWeather from './components/CurrentWeather';
 import { useEffect, useState } from "react";
-import { url, options } from "./data/data";
+import { url, options, weatherCodeDay } from "./data/data";
 import ConditionsList from './components/ConditionsList';
 import ForecastList from './components/ForecastList';
 
 function App() {
   const initialWeatherState = {
-    daily: [],
+    current: {},
     hourly: [],
-    current: [],
+    daily: [],
   }
   const [loading, setLoading] = useState(true);
   const [weather, setWeather] = useState(initialWeatherState);
@@ -19,8 +19,20 @@ function App() {
       .then(response => response.json())
       .then((response) => {
         // check if response.code is 429001 -> show error message that the request limit for this resource has been reached. Please wait and try again in an hour. Thank you for your patience
-        console.log(response.data);
-        setWeather({ ...weather, daily: response.data.timelines[0].intervals, hourly: response.data.timelines[1].intervals.slice(0, 13), })
+        console.log(response.code);
+        setWeather({
+          ...weather,
+          current: {
+            ...weather.current,
+            weatherCodeDay: response.data.timelines[0].intervals[0].values.weatherCodeDay,
+            temperature: response.data.timelines[1].intervals[0].values.temperature,
+            temperatureMax: response.data.timelines[0].intervals[0].values.temperatureMax,
+            temperatureMin: response.data.timelines[0].intervals[0].values.temperatureMin,
+            hourlyConditions: [response.data.timelines[1].intervals[0].values],
+          },
+          hourly: response.data.timelines[1].intervals.slice(0, 24),
+          daily: response.data.timelines[0].intervals,
+        })
         setLoading(false)
       })
       .catch(err => console.error(err));
