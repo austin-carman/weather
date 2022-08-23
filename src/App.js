bc935d5eb5d07fac7279ae12a1814c6695dec90e
Austin Carman <abcarman12@gmail.com>
2022-08-23 08:51:42 -1000

    state is properly passed and displayed to CurrentWeather

--- src/App.js
+++ src/App.js
@@ -19,15 +19,14 @@ function App() {
       .then(response => response.json())
       .then((response) => {
         // check if response.code is 429001 -> show error message that the request limit for this resource has been reached. Please wait and try again in an hour. Thank you for your patience
-        console.log(response.code);
         setWeather({
           ...weather,
           current: {
             ...weather.current,
-            weatherCodeDay: response.data.timelines[0].intervals[0].values.weatherCodeDay,
-            temperature: response.data.timelines[1].intervals[0].values.temperature,
-            temperatureMax: response.data.timelines[0].intervals[0].values.temperatureMax,
-            temperatureMin: response.data.timelines[0].intervals[0].values.temperatureMin,
+            weatherCodeDay: weatherCodeDay[response.data.timelines[0].intervals[0].values.weatherCodeDay],
+            temperature: Math.floor(response.data.timelines[1].intervals[0].values.temperature),
+            temperatureMax: Math.floor(response.data.timelines[0].intervals[0].values.temperatureMax),
+            temperatureMin: Math.floor(response.data.timelines[0].intervals[0].values.temperatureMin),
             hourlyConditions: [response.data.timelines[1].intervals[0].values],
           },
           hourly: response.data.timelines[1].intervals.slice(0, 24),
@@ -50,7 +49,7 @@ function App() {
           <div>Loading...</div>
         ) : (
           <div>
-            <CurrentWeather weather={weather} />
+            <CurrentWeather weather={weather.current} />
             <ConditionsList weather={weather} setCondition={setCondition} />
             <ForecastList weather={weather} condition={condition} />
           </div>
