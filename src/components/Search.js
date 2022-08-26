b42d208a8972454e45ab2fae88bc1ffbd72468d8
Austin Carman <abcarman12@gmail.com>
2022-08-25 16:56:02 -1000

    connected to mapbox api for searching location

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,16 +1,43 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function Search(props) {
   const setCity = props.setCity;
-  const [userInput, setUserInput] = useState("");
+  const [searchText, setSearchText] = useState("");
+  const [searchSuggestions, setSearchSuggestions] = useState([]);
+
+  const mapboxApiKey = process.env.REACT_APP_MAPBOX_KEY;
+  const minQueryLength = 3;
+  const baseURL = "https://api.mapbox.com/geocoding/v5/mapbox.places/";
+  const dynamicURL = `${searchText}.json?country=us&types=place%2Cpostcode%2Caddress&language=en&access_token=${mapboxApiKey}`;
+  const options = {
+    method: "GET",
+    headers: {
+      Accept: "application/json, text/plain, */*",
+      "Access-Control-Allow-Origin": "*",
+      "Content-Type": "application/json;charset=UTF-8",
+    }
+  };
+
+  useEffect(() => {
+    if (searchText.length >= minQueryLength) {
+      fetch(baseURL + dynamicURL, options)
+        .then((res) => res.json())
+        .then((data) => setSearchSuggestions(data.features))
+        .catch((err) => {
+          console.log("error: ", err);
+        })
+    }
+
+  }, [searchText]);
 
   const handleChange = (e) => {
-    setUserInput(e.target.value)
+    setSearchText(e.target.value);
   }
 
   const handleSubmit = () => {
-    setCity(userInput);
-    setUserInput("");
+    // submitting without clicking the auto suggested submits the top suggestion
+    setCity(searchText);
+    setSearchText("");
   }
 
   return (
@@ -19,9 +46,12 @@ function Search(props) {
         type="text"
         placeholder="Search City or Zip"
         onChange={handleChange}
-        value={userInput}
+        value={searchText}
       />
       <button onClick={handleSubmit}>Submit</button>
+      {searchSuggestions.map((city) => {
+        return <div>{city.place_name}</div>
+      })}
     </>
   )
 }
