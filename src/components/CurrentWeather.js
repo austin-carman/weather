d946c5a50e6c5e35d51b28ef10ebbc3f3fb4184a
Austin Carman <abcarman12@gmail.com>
2022-09-13 13:49:01 -1000

    removed commented out code that is no longer needed

--- src/components/CurrentWeather.js
+++ src/components/CurrentWeather.js
@@ -1,12 +1,10 @@
 import { weatherCodes } from "../data/data";
-// import { weatherDayCodes, weatherNightCodes } from "../data/data";
 
-// Displays current temp/temp low/temp high
+// Displays major current conditions (icon, description, temp/temp low/temp high)
 function CurrentWeather(props) {
   const { currentHourWeather, currentDayWeather, city } = props;
-
-  // To get current time (hour) as a number on a 1-24 hour scale
   const date = new Date(currentHourWeather.startTime);
+  // Get current hour on a 1-24 hour scale
   const time = date.toLocaleTimeString([], { hour: 'numeric', hour12: false });
   // If current time is between 5:00am and 10pm use weatherCodyDay otherwise use weatherCodeNight
   const code = (parseInt(time) > 5 && parseInt(time) < 22) ? currentDayWeather.weatherCodeDay : currentDayWeather.weatherCodeNight;
