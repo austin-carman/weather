740f039dc1d64c7358d9d2737c59c6c0ecc2ad29
Austin Carman <abcarman12@gmail.com>
2022-09-15 09:44:58 -1000

    styled Home.js, image placement

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,10 @@
 function Home() {
   return (
-    <div>
-      <img src="icons/large/png/10000.png" alt="Sunny" />
-      <img src="icons/large/png/40010.png" alt="Rainy" />
-      <img src="icons/large/png/11010.png" alt="Partly Cloudy" />
-      <img src="icons/large/png/50000.png" alt="Snowy" />
+    <div className="home-icons-container">
+      <img className="item1" src="icons/large/png/10000.png" alt="Sun" />
+      <img className="item2" src="icons/large/png/40010.png" alt="Rain clouds" />
+      <img className="item3" src="icons/large/png/11010.png" alt="Sun with clouds" />
+      <img className="item4" src="icons/large/png/50000.png" alt="Snow clouds" />
     </div>
   )
 }
